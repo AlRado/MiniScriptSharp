d532074dae4ec1d6d358f83460b6d5133e789fd9
alrado <alrado08@gmail.com>
2021-05-10 14:12:07 +0300

    minor changes

--- MiniScript-cs/Miniscript/intrinsic/Result.cs
+++ MiniScript-cs/Miniscript/intrinsic/Result.cs
@@ -10,6 +10,33 @@ namespace Miniscript.intrinsic {
 		/// then your intrinsic will get invoked with this Result passed in as partialResult).
 		/// </summary>
 		public class Result {
+
+			/// <summary>
+			/// Result.Null: static Result representing null (no value).
+			/// </summary>
+			public static readonly Result Null = new Result(null, true);
+
+			/// <summary>
+			/// Result.EmptyString: static Result representing "" (empty string).
+			/// </summary>
+			public static readonly Result EmptyString = new Result(ValString.Empty);
+
+			/// <summary>
+			/// Result.True: static Result representing true (1.0).
+			/// </summary>
+			public static readonly Result True = new Result(ValNumber.One, true);
+
+			/// <summary>
+			/// Result.True: static Result representing false (0.0).
+			/// </summary>
+			public static readonly Result False = new Result(ValNumber.Zero, true);
+
+			/// <summary>
+			/// Result.Waiting: static Result representing a need to wait,
+			/// with no in-progress value.
+			/// </summary>
+			public static readonly Result Waiting = new Result(null, false);
+			
 			public readonly bool Done;		// true if our work is complete; false if we need to Continue
 			public readonly Value ResultValue;	// final result if Done; in-progress data if not Done
 			
@@ -38,33 +65,5 @@ namespace Miniscript.intrinsic {
 				this.Done = true;
 				this.ResultValue = string.IsNullOrEmpty(resultStr) ? ValString.Empty : new ValString(resultStr);
 			}
-			
-			/// <summary>
-			/// Result.Null: static Result representing null (no value).
-			/// </summary>
-			public static readonly Result Null = new Result(null, true);
-
-			/// <summary>
-			/// Result.EmptyString: static Result representing "" (empty string).
-			/// </summary>
-			public static readonly Result EmptyString  = new Result(ValString.Empty);
-
-			/// <summary>
-			/// Result.True: static Result representing true (1.0).
-			/// </summary>
-			public static readonly Result True = new Result(ValNumber.One, true);
-
-			/// <summary>
-			/// Result.True: static Result representing false (0.0).
-			/// </summary>
-			public static readonly Result False = new Result(ValNumber.Zero, true);
-
-			/// <summary>
-			/// Result.Waiting: static Result representing a need to wait,
-			/// with no in-progress value.
-			/// </summary>
-			public static readonly Result Waiting = new Result(null, false);
-
 		}
-
 }
