95b2b90cc2f80f51a0022870c5711b484254b65a
alrado <alrado08@gmail.com>
2021-05-22 14:16:53 +0300

    progress: перевести все комментарии в атрибуты описания методов

--- Miniscript/intrinsic/IntrinsicFunctions.cs
+++ Miniscript/intrinsic/IntrinsicFunctions.cs
@@ -14,106 +14,124 @@ namespace Miniscript.intrinsic {
         
         private static Random random = new Random();
         
-        // abs
-        //	Returns the absolute value of the given number.
-        // x (number, default 0): number to take the absolute value of.
-        // Example: abs(-42)		returns 42
+        [Description(
+            "\n   Returns the absolute value of the given number." +
+            "\n x (number, default 0): number to take the absolute value of." +
+            "\n Example: abs(-42)		returns 42" +
+            "\n"
+        )]
         public double Abs(double x = 0) {
             return Math.Abs(x);
         }
         
-        // acos
-        //	Returns the inverse cosine, that is, the angle 
-        //	(in radians) whose cosine is the given value.
-        // x (number, default 0): cosine of the angle to find.
-        // Returns: angle, in radians, whose cosine is x.
-        // Example: acos(0) 		returns 1.570796
+        [Description(
+            "\n   Returns the inverse cosine, that is, the angle" +
+            "\n   (in radians) whose cosine is the given value." +
+            "\n x (number, default 0): cosine of the angle to find." +
+            "\n Returns: angle, in radians, whose cosine is x." +
+            "\n Example: acos(0) 		returns 1.570796" +
+            "\n"
+        )]
         public double Acos(double x = 0) {
             return Math.Acos(x);
         }
         
-        // asin
-        //	Returns the inverse sine, that is, the angle
-        //	(in radians) whose sine is the given value.
-        // x (number, default 0): cosine of the angle to find.
-        // Returns: angle, in radians, whose cosine is x.
-        // Example: asin(1) return 1.570796
+        [Description(
+            "\n   Returns the inverse sine, that is, the angle" +
+            "\n   (in radians) whose sine is the given value." +
+            "\n x (number, default 0): cosine of the angle to find." +
+            "\n Returns: angle, in radians, whose cosine is x." +
+            "\n Example: asin(1) return 1.570796" +
+            "\n"
+        )]
         public double Asin(double x = 0) {
             return Math.Asin(x);
         }
         
-        // atan
-        //	Returns the arctangent of a value or ratio, that is, the
-        //	angle (in radians) whose tangent is y/x.  This will return
-        //	an angle in the correct quadrant, taking into account the
-        //	sign of both arguments.  The second argument is optional,
-        //	and if omitted, this function is equivalent to the traditional
-        //	one-parameter atan function.  Note that the parameters are
-        //	in y,x order.
-        // y (number, default 0): height of the side opposite the angle
-        // x (number, default 1): length of the side adjacent the angle
-        // Returns: angle, in radians, whose tangent is y/x
-        // Example: atan(1, -1)		returns 2.356194
+        [Description(
+            "\n   Returns the arctangent of a value or ratio, that is, the" +
+            "\n   angle (in radians) whose tangent is y/x.  This will return" +
+            "\n   an angle in the correct quadrant, taking into account the" +
+            "\n   sign of both arguments.  The second argument is optional," +
+            "\n   and if omitted, this function is equivalent to the traditional" +
+            "\n   one-parameter atan function.  Note that the parameters are" +
+            "\n   in y,x order." +
+            "\n y (number, default 0): height of the side opposite the angle" +
+            "\n x (number, default 1): length of the side adjacent the angle" +
+            "\n Returns: angle, in radians, whose tangent is y/x" +
+            "\n Example: atan(1, -1)		returns 2.356194" +
+            "\n"
+        )]
         public double Atan(double y = 0, double x = 1) {
             return x == 1.0 ? Math.Atan(y) : Math.Atan2(y, x);
         }
         
-        // bitAnd
-        //	Treats its arguments as integers, and computes the bitwise
-        //	`and`: each bit in the result is set only if the corresponding
-        //	bit is set in both arguments.
-        // i (number, default 0): first integer argument
-        // j (number, default 0): second integer argument
-        // Returns: bitwise `and` of i and j
-        // Example: bitAnd(14, 7)		returns 6
-        // See also: bitOr; bitXor
+        [Description(
+            "\n   Treats its arguments as integers, and computes the bitwise" +
+            "\n   `and`: each bit in the result is set only if the corresponding" +
+            "\n   bit is set in both arguments." +
+            "\n i (number, default 0): first integer argument" +
+            "\n j (number, default 0): second integer argument" +
+            "\n Returns: bitwise `and` of i and j" +
+            "\n Example: bitAnd(14, 7)		returns 6" +
+            "\n See also: bitOr; bitXor" +
+            "\n"
+        )]
         public int BitAnd(int i, int j) {
             return i & j;
         }
         
-        // bitOr
-        //	Treats its arguments as integers, and computes the bitwise
-        //	`or`: each bit in the result is set if the corresponding
-        //	bit is set in either (or both) of the arguments.
-        // i (number, default 0): first integer argument
-        // j (number, default 0): second integer argument
-        // Returns: bitwise `or` of i and j
-        // Example: bitOr(14, 7)		returns 15
-        // See also: bitAnd; bitXor
+        [Description(
+            "\n   Treats its arguments as integers, and computes the bitwise" +
+            "\n   `or`: each bit in the result is set if the corresponding" +
+            "\n   bit is set in either (or both) of the arguments." +
+            "\n i (number, default 0): first integer argument" +
+            "\n j (number, default 0): second integer argument" +
+            "\n Returns: bitwise `or` of i and j" +
+            "\n Example: bitOr(14, 7)		returns 15" +
+            "\n See also: bitAnd; bitXor" +
+            "\n"
+        )]
         public int BitOr(int i, int j) {
             return i | j;
         }
         
-        // bitXor
-        //	Treats its arguments as integers, and computes the bitwise
-        //	`xor`: each bit in the result is set only if the corresponding
-        //	bit is set in exactly one (not zero or both) of the arguments.
-        // i (number, default 0): first integer argument
-        // j (number, default 0): second integer argument
-        // Returns: bitwise `and` of i and j
-        // Example: bitAnd(14, 7)		returns 9
-        // See also: bitAnd; bitOr
+        [Description(
+            "\n   Treats its arguments as integers, and computes the bitwise" +
+            "\n   `xor`: each bit in the result is set only if the corresponding" +
+            "\n   bit is set in exactly one (not zero or both) of the arguments." +
+            "\n i (number, default 0): first integer argument" +
+            "\n j (number, default 0): second integer argument" +
+            "\n Returns: bitwise `and` of i and j" +
+            "\n Example: bitAnd(14, 7)		returns 9" +
+            "\n See also: bitAnd; bitOr" +
+            "\n"
+        )]
         public int BitXor(int i, int j) {
             return i ^ j;
         }
         
-        // char
-        //	Gets a character from its Unicode code point.
-        // codePoint (number, default 65): Unicode code point of a character
-        // Returns: string containing the specified character
-        // Example: char(42)		returns "*"
-        // See also: code
+        [Description(
+            "\n   Gets a character from its Unicode code point." +
+            "\n   codePoint (number, default 65): Unicode code point of a character" +
+            "\n Returns: string containing the specified character" +
+            "\n Example: char(42)		returns \"*\"" +
+            "\n See also: code" +
+            "\n"
+        )]
         public string Char(int codePoint = 65) {
             return char.ConvertFromUtf32(codePoint);
         }
         
-        // ceil
-        //	Returns the "ceiling", i.e. closest whole number 
-        //	greater than or equal to the given number.
-        // x (number, default 0): number to get the ceiling of
-        // Returns: closest whole number not less than x
-        // Example: ceil(41.2)		returns 42
-        // See also: floor
+        [Description(
+            "\n   Returns the \"ceiling\", i.e. closest whole number" +
+            "\n   greater than or equal to the given number." +
+            "\n x (number, default 0): number to get the ceiling of" +
+            "\n Returns: closest whole number not less than x" +
+            "\n Example: ceil(41.2)		returns 42" +
+            "\n See also: floor" +
+            "\n"
+        )]
         public double Ceil(double x = 0) {
             return Math.Ceiling(x);
         }
